608981297da0d9b6d85b90075ef9be61eaf5b028
GlorifiedPig <ryan@glorifiedstudios.com>
2021-03-01 10:34:24 +0200

    push vector.lua

---
+++ vector.lua
@@ -0,0 +1,47 @@
+
+Vector = {}
+Vector.__index = Vector
+
+local function newVector( x, y )
+    return setmetatable( { x = x or 0, y = y or 0 }, Vector )
+end
+
+function isvector( vTbl )
+    return getmetatable( vTbl ) == Vector
+end
+
+function Vector.__unm( vTbl )
+    return newVector( -vTbl.x, -vTbl.y )
+end
+
+function Vector.__add( a, b )
+    return newVector( a.x + b.x, a.y + b.y )
+end
+
+function Vector.__sub( a, b )
+    return newVector( a.x - b.x, a.y - b.y )
+end
+
+function Vector.__mul( a, b )
+    if type( a ) == "number" then
+        return newVector( a * b.x, a * b.y )
+    elseif type( b ) == "number" then
+        return newVector( a.x * b, a.y * b )
+    else
+        return newVector( a.x * b.x, a.y * b.y )
+    end
+end
+
+function Vector.__div( a, b )
+    return newVector( a.x / b, a.y / b )
+end
+
+function Vector.__eq( a, b )
+    return a.x == b.x and a.y == b.y
+end
+
+function Vector:__tostring()
+    return "(" .. self.x .. ", " .. self.y .. ")"
+end
+
+return setmetatable( Vector, { __call = function( _, ... ) return newVector( ... ) end } )
